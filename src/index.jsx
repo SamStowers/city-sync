cd2cf4e35278fc80eb563d808349eaf448898ff9
Sam Stowers <samuelstowers04@gmail.com>
2025-02-16 18:28:39 -0500

    Sprint 2

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client"
+import 'bootstrap/dist/css/bootstrap.min.css';
 const root = createRoot(document.getElementById("root"))
 
 function Navbar() {
@@ -39,9 +40,40 @@ function Navbar() {
     )
 }
 
+function SignIn() {
+    return(
+        <main className="form-signin w-100 m-auto">
+            <form>
+                {/* <img class="mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"> */}
+                <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+
+                <div className="form-floating">
+                    <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
+                    <label htmlFor="floatingInput">Email address</label>
+                </div>
+                <div className="form-floating">
+                    <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
+                    <label htmlFor="floatingPassword">Password</label>
+                </div>
+
+                <div className="form-check text-start my-3">
+                    <input className="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault"/>
+                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                        Remember me
+                    </label>
+                </div>
+                <button className="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+                <p className="mt-5 mb-3 text-body-secondary">&copy; 2025–2025</p>
+            </form>
+        </main>
+    )
+}
+
 root.render(
     <div>
         <Navbar />
-        <h1>Testing!!!</h1>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <SignIn style={{ maxWidth: '400px', width: '100%' }} />
+        </div>
     </div>
 )
